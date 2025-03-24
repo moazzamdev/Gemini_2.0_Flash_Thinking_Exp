ffff46d0bad35a64859a9ac55bd38f76117a8be3
Moazzam Riaz <126562859+moazzamdev@users.noreply.github.com>
2025-03-25 02:20:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from page1 import text
+from page2 import image
+from page3 import details
+
+
+def main():
+
+    #st.title("Gemini 2.0 Thinking Experimental")
+
+    with st.sidebar:
+        selection = option_menu(
+        menu_title="Main Menu",
+        options=["About Gemini","Text + Image Queries", "Image Generation"],
+        icons=["book","pencil", "image"],
+        menu_icon="cast",
+        default_index=1
+        )
+
+    if selection == "Text + Image Queries":
+        text()
+
+    elif selection == "Image Generation":
+        image()
+
+    elif selection == "About Gemini":
+        details()
+
+
+if __name__ == '__main__':
+    main()
